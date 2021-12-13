baf45b96214524b7437dc77d6ba08a41fd00a01f
kote1990 <kote25081990@gmail.com>
2021-12-13 14:33:04 -0800

    up

--- lib/compliterhtml.rb
+++ lib/compliterhtml.rb
@@ -1,25 +1,36 @@
-class GetHtml
-  def get(content, bypass_html = true, file_name = 'index.html')
-    markup = gets.chomp
-    markup = content.gsub!(/[<>]/, '') if bypass_html
-#    markup = content if bypass_html
-    
+require 'nokogiri'
+
+class MakeHtml
+  def make_html(content, bypass_html, file_name = 'index.html')
+    markup = content.gsub!(/[<>]/, '') if bypass_html == false
+    markup = content unless bypass_html == false
+
     f = File.new("#{Dir.pwd}/#{file_name}", "w+")
-    f.puts "<!DOCTYPE HTML>"
-    f.puts '<html lang="en">'
+    f.puts "<!DOCTYPE html>"
     f.puts "  <head>"
-    f.puts '    <meta charset="UTF-8">'
-    f.puts "    <title>My Pet</title>"
+    f.puts "    <meta charset='utf-8'>"
+    f.puts "    <title>My Tamago</title>"
     f.puts "  </head>"
     f.puts "  <body>"
-    f.puts "    <p>#{content}</p>"
+    f.puts "    <script>"
+    f.puts "      setInterval(()=>{ window.location.reload() }, 2000)"
+    f.puts "    </script>"
+    f.puts "    #{markup}"
     f.puts "  </body>"
     f.puts "</html>"
     f.close
-
   end
 
-  HTML
-  File.open(output_file, 'w') { |f| f.write file_html }
-end
+  def update_html(content, file_name = 'index.html')
+    doc = File.open(file_name) { |f| Nokogiri::HTML(f) }
+    doc.at('body') << content
 
+    file = File.open(file_name, 'w+')
+    file.puts doc
+    file.close
+  end
+
+  def open_in_browser(file = 'index.html')
+    system("xdg-open #{Dir.pwd}/#{file}")
+  end
+end
